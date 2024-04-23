c545877125dd1b42979cf89b9e1d4aca0d94de47
harsh-1214 <harshkhasgiwale@gmail.com>
2024-04-23 08:06:57 +0530

    second-commit

--- weka-clone/src/App.jsx
+++ weka-clone/src/App.jsx
@@ -57,28 +57,50 @@ function App() {
   }
 
   function handleSubmit(e) {
+    console.log(file)
     e.preventDefault();
     numericCorellation();
   }
 
   return (
-    <div style={{ textAlign: "center", margin: "100px" }}>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          name="arffData"
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-        <input
-          type="text"
-          value={firstVal}
-          onChange={(e) => setFirstVal(e.target.value)}
-        />
-        <input
-          type="text"
-          value={secondVal}
-          onChange={(e) => setSecondVal(e.target.value)}
-        />
+    <div className="flex justify-center items-center flex-col">
+      <form onSubmit={handleSubmit} className=" bg-blue-400 p-4 flex flex-col items-center">
+        
+        <label htmlFor="arffData" className="flex cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+          </svg>
+          Upload
+          <input
+            type="file"
+            name="arffData"
+            id="arffData"
+            onChange={(e) => setFile(e.target.files[0])}
+            className="cursor-pointer hidden"
+          />
+        </label>
+        {
+          <div>
+            {file.name}
+          </div>
+        }
+        <div className="flex justify-between w-full">
+          <div>First Attibute</div>
+          <div>Second Attibute</div>
+        </div>
+        <div className=""> 
+          <input
+            type="text"
+            value={firstVal}
+            onChange={(e) => setFirstVal(e.target.value)}
+            className="m-2"
+          />
+          <input
+            type="text"
+            value={secondVal}
+            onChange={(e) => setSecondVal(e.target.value)}
+          />
+        </div>
         <button type="submit"> Upload File</button>
       </form>
 
